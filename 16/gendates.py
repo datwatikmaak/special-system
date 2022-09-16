78b01834f63457eb5707f45b9e8c9b9d0808b030
Leonieke <datwatikmaak@gmail.com>
2022-09-16 04:04:56 +0200

    submission Bite 16 @ codechalleng.es

--- 16/gendates.py
+++ 16/gendates.py
@@ -4,4 +4,10 @@ PYBITES_BORN = datetime(year=2016, month=12, day=19)
 
 
 def gen_special_pybites_dates():
-    pass
+    base = PYBITES_BORN + timedelta(days=100)
+    while True:
+        yield base
+        base = base + timedelta(days=100)
+
+
+gen_special_pybites_dates()
