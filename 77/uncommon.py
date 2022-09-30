b3e0baccac45587fd005772213aeb40d990886f8
Leonieke <datwatikmaak@gmail.com>
2022-09-30 10:21:14 +0200

    submission Bite 77 @ codechalleng.es

--- 77/uncommon.py
+++ 77/uncommon.py
@@ -1,4 +1,14 @@
 def uncommon_cities(my_cities, other_cities):
     """Compare my_cities and other_cities and return the number of different
-       cities between the two"""
-    pass
+    cities between the two"""
+    diff = list(
+        list(set(my_cities) - set(other_cities))
+        + list(set(other_cities) - set(my_cities))
+    )
+    return len(diff)
+
+
+uncommon_cities(
+    ["Rome", "Paris", "Madrid", "Chicago", "Seville", "Berlin"],
+    ["Paris", "Boston", "Sydney", "Madrid", "Moscow", "Lima"],
+)  # 8
