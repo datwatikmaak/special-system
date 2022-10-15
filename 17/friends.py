8dcbd5f9616b206c06c5785ef2d613b91c77cf13
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2022-10-15 05:42:06 +0200

    submission Bite 17 @ codechalleng.es

--- 17/friends.py
+++ 17/friends.py
@@ -1,2 +1,8 @@
-def friends_teams():
-    pass
+from itertools import combinations, permutations
+
+
+def friends_teams(friends, team_size=2, order_does_matter=False):
+    if order_does_matter:
+        return list(permutations(friends, team_size))
+    else:
+        return list(combinations(friends, team_size))
