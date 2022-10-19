719727be2657025496671afabff1e2939ac9c153
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2022-10-19 04:25:58 +0200

    submission Bite 39 @ codechalleng.es

--- 39/coursetime.py
+++ 39/coursetime.py
@@ -5,31 +5,36 @@ from typing import List
 import urllib.request
 
 # getting the data
-COURSE_TIMES = os.path.join(
-    os.getenv("TMP", "/tmp"),
-    'course_timings'
-)
+COURSE_TIMES = os.path.join(os.getenv("TMP", "/tmp"), "course_timings")
 urllib.request.urlretrieve(
-    'https://bites-data.s3.us-east-2.amazonaws.com/course_timings',
-    COURSE_TIMES
+    "https://bites-data.s3.us-east-2.amazonaws.com/course_timings", COURSE_TIMES
 )
 
 
 def get_all_timestamps() -> List[str]:
     """Read in the COURSE_TIMES and extract all MM:SS timestamps.
-       Here is a snippet of the input file:
+    Here is a snippet of the input file:
 
-       Start  What is Practical JavaScript? (3:47)
-       Start  The voice in your ear (4:41)
-       Start  Is this course right for you? (1:21)
-       ...
+    Start  What is Practical JavaScript? (3:47)
+    Start  The voice in your ear (4:41)
+    Start  Is this course right for you? (1:21)
+    ...
 
-        Return a list of MM:SS timestamps
+     Return a list of MM:SS timestamps
     """
-    pass
+    with open(COURSE_TIMES) as file:
+        content = " ".join(file.read().splitlines()).split()
+
+    return [line.strip("()") for line in content if line.startswith("(")]
 
 
 def calc_total_course_duration(timestamps) -> str:
     """Takes timestamps list as returned by get_all_timestamps
-       and calculates the total duration as HH:MM:SS"""
-    pass
+    and calculates the total duration as HH:MM:SS"""
+    total_time = timedelta()
+    for t in timestamps:
+        (m, s) = t.split(":")
+        d = timedelta(minutes=int(m), seconds=int(s))
+        total_time += d
+
+    return str(total_time)
