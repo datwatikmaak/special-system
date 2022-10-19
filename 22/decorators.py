1d7e29b59567028992ffc132d331f2e02e73c6ad
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2022-10-19 03:38:42 +0200

    submission Bite 22 @ codechalleng.es

--- 22/decorators.py
+++ 22/decorators.py
@@ -2,4 +2,11 @@ from functools import wraps
 
 
 def make_html(element):
-    pass
+    def inner_func(func):
+        @wraps(func, element)
+        def wrapper(*args, **kwargs):
+            return f"<{element}>{func(*args, **kwargs)}</{element}>"
+
+        return wrapper
+
+    return inner_func
